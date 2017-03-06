06ab00b919d229129f4e72c01e97296dc9cd9024
Jackson Morgan <jmorgan@stuy.edu>
2017-03-06 11:31:49 -0500

    some tests

--- main.c
+++ main.c
@@ -14,6 +14,34 @@ int main(int argc, char **argv) {
   struct matrix * edges;
   struct matrix * transform;
 
+  struct matrix * edges3 =  new_matrix(4, 4);
+
+  
+  color c;
+ 
+  
+  c.red = 0;
+  c.green = MAX_COLOR;
+  c.blue = 0;
+
+
+  add_edge(edges3,150,80,0,250,80,0);
+  add_edge(edges3,150,80,0,150,280,0);
+  add_edge(edges3,250,80,0,250,280,0);
+  add_edge(edges3,150,280,0,250,280,0);
+  add_edge(edges3,150,280,0,200,330,0);
+  add_edge(edges3,250,280,0,200,330,0);
+  add_edge(edges3,250,80,0,300,120,0);
+  add_edge(edges3,250,280,0,300,320,0);
+  add_edge(edges3,300,320,0,300,120,0);
+  add_edge(edges3,200,330,0,250,370,0);
+  add_edge(edges3,300,320,0,250,370,0);
+
+  matrix_mult(make_rotZ(60),edges3);
+  draw_lines(edges3,s,c);
+  display(s);
+ save_extension(s, "matrix.png");
+
   edges = new_matrix(4, 4);
   transform = new_matrix(4, 4);
 
