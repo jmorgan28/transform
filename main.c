e5e90121d62b8fa7e8dd604eca7c0de3374edf15
Jackson Morgan <jacksonmorgan@Jacksons-MacBook-Pro.local>
2017-03-06 22:16:32 -0500

    something messed up

--- main.c
+++ main.c
@@ -37,10 +37,10 @@ int main(int argc, char **argv) {
   add_edge(edges3,200,330,0,250,370,0);
   add_edge(edges3,300,320,0,250,370,0);
 
-  //matrix_mult(make_rotX(60),edges3);
+  matrix_mult(make_scale(2,2,2),edges3);
   draw_lines(edges3,s,c);
   display(s);
-  save_extension(s, "matrix.png");
+  save_extension(s, "matri.png");
 
   edges = new_matrix(4, 4);
   transform = new_matrix(4, 4);
